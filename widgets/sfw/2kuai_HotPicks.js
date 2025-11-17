7ac6660bb10e796948ee635ddb00a3dc7d5b0d4b
syunraii <syunraii@outlook.com>
2025-11-18 02:18:26 +0800

    test

--- widgets/sfw/2kuai_HotPicks.js
+++ widgets/sfw/2kuai_HotPicks.js
@@ -1,7 +1,7 @@
 ﻿const USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36";
 
 var WidgetMetadata = {
-  id: "hot_picks",
+  id: "alter.hot_picks",
   title: "热门精选",
   description: "获取最新热门影片推荐",
   author: "两块",
